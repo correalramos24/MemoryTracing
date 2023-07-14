32a27de10b06420724b4a9e7d3768512604d0601
Victor Correal <victor.correal@bsc.es>
2023-07-14 11:08:36 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SAMPLING_TIME=2
+MEM_FILE="mem.log"
+
+echo "Enable mem tracing with sampling every $SAMPLING_TIME"
+
+free -s $SAMPLING_TIME &> $MEM_FILE &
+PID_FREE=$!
+
+#Run your program:
+sleep 10
+
+#Stop memory tracing
+echo "Stop memory tracing. Results @ $MEM_FILE (units in KiB)"
+kill $PID_FREE
+
+echo "All done"
